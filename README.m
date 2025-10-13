730b71d046b78c237b7f99ef32f51186aca5cd5f
Vidit <viditsharma@SSLMTADMINs-MacBook-Air-2.local>
2025-10-13 23:28:32 +0530

    Remove README.m

--- README.m
+++
@@ -1,6 +0,0 @@
-# Linux-Commands
-These are the different commands mostly use for linux. 
-most of the commands for the linux are here. i'll put more as i am going. but the git and basic linux commands i have put.
-'''
-From a begineer level, these commands will help to learn how to use linux
-'''
