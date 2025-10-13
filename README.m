c92b987145c2800a80e54b8839dfe843761d3222
Vidit <viditsharma@SSLMTADMINs-MacBook-Air-2.local>
2025-10-13 23:24:56 +0530

    new changes in readme

--- README.m
+++ README.m
@@ -1,7 +1,6 @@
 # Linux-Commands
 These are the different commands mostly use for linux. 
-
-all the new commands are added ranging from the basic linux commands to setting up github.
+most of the commands for the linux are here. i'll put more as i am going. but the git and basic linux commands i have put.
 '''
 From a begineer level, these commands will help to learn how to use linux
 '''
